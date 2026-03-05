6d2d454b4999695d8055c3fba0c59b0bab5aa79b
abdukodir <abdukodirsheraliev1@gmail.com>
2026-03-06 00:19:54 +0900

    feat: integration GraphQlModule and build first GraphQL api

--- apps/near-help/src/app.service.ts
+++ apps/near-help/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
 	getHello(): string {
-		return 'Welcome to NearHelp API Server!';
+		return 'Welcome to NearHelp Rest API Server!';
 	}
 }
